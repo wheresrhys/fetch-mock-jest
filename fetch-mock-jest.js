4c81713836aa0daa33cad9bfb8b6b92725e6dbbd
Rhys Evans <rhys.evans@ft.com>
2019-12-19 09:11:32 +0000

    add proxynig the sandbox method back in

--- fetch-mock-jest.js
+++ fetch-mock-jest.js
@@ -11,18 +11,49 @@ module.exports = Object.assign(fetchMock, {
 	Headers: nodeFetch.Headers,
 });
 
+
+require('./jest-extensions');
+
+const nodeFetch = jest.requireActual('node-fetch');
+const fetchMock = require('../fetch-mock-jest').sandbox();
+Object.assign(fetchMock.config, nodeFetch, {
+  fetch: nodeFetch
+});
+module.exports = Object.assign(fetchMock, {
+	Request: nodeFetch.Request,
+	Response: nodeFetch.Response,
+	Headers: nodeFetch.Headers,
+});
+
 const fetchMock = require('fetch-mock');
 const jestify = fetchMockInstance => {
 
+		const jestifiedInstance = new Proxy(fetchMockInstance, {
+			get: (originalFetchMock, name) => {
+				if (name === 'sandbox') {
+					return new Proxy(originalFetchMock[name], {
+						apply: (func, thisArg, args) => {
+							const sandboxedFetchMock = func.apply(originalFetchMock, args);
+							return jestify(sandboxedFetchMock);
+						}
+					});
+				}
+				return originalFetchMock[name];
+			}
+		});
+
 	// spy on the fetch handler so we can use all the
 	// jest function assertions on it
-	jest.spyOn(fetchMockInstance, 'fetchHandler')
+	jest.spyOn(jestifiedInstance, 'fetchHandler')
 
 	// make sure all the jest expectation helpers can find what they need on fetchMock.mock
-	Object.assign(fetchMockInstance.mock, fetchMockInstance.fetchHandler.mock)
+	Object.assign(jestifiedInstance.mock, jestifiedInstance.fetchHandler.mock)
+
+	jestifiedInstance.fetchHandler.mock = jestifiedInstance.mock;
 
 	// Return this monster!
-	return fetchMockInstance
+	return jestifiedInstance
 }
 
 module.exports = jestify(fetchMock)
+
